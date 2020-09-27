b74aeef7dc642745c77ca2ef95804f0ce5c10646
Michael E. Karpeles (mek) <michael.karpeles@gmail.com>
2020-09-27 12:51:40 -0700

    Fixed auth to fetch OL username

--- bestbook/api/auth.py
+++ bestbook/api/auth.py
@@ -9,23 +9,21 @@ def login(email, password):
     """Authenticates user and returns session"""
 
     email = email.replace(' ', '+')
-    
+
     # We're already logged in, so return a True value
     if session.get('username'):
         return session.get('username')
 
     try:
-        # This authenticates credentials or throws an error
-
         response = get_auth_config(email, password)
-
-        # Here, we now know the user's credentials are correct.
-        # Next, we need to get their OpenLibrary username
-        session['username'] = email
         
-        # Aasif, can you please look into how to get a patron's Open
-        # Library username if they're logged in?
-        return session['username']
+        # We now know the user's credentials are correct.
+        # Next, we need to get their OpenLibrary username        
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post('https://openlibrary.org/account/login', headers=headers, json=response.get('s3'))
+        username = r.cookies.get('session').split('/')[2].split('%2C')[0]        
+        session['username'] = username
+        return username
 
     except AuthenticationError:
         return False
